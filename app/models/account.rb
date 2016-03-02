863afd17f10db63f0f2b68716c294f00dd265941
Publicover <jameslpublicover@gmail.com>
2016-03-02 15:26:14 -0500

    pass balance method test

--- app/models/account.rb
+++ app/models/account.rb
@@ -1,2 +1,7 @@
 class Account < ActiveRecord::Base
+
+  def self.display_balance
+    self.all.reduce(0.0) {|x, y| x + y.amount }
+  end
+
 end
--- test/models/account_test.rb
+++ test/models/account_test.rb
@@ -1,7 +1,11 @@
 require 'test_helper'
 
 class AccountTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "the truth" do
+    assert true
+  end
+
+  test "should display balance" do
+    assert_equal 7.0, Account.display_balance
+  end
 end
