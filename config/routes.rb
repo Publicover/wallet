5fa1b77921c7af5de4961dec24fa5411fc6f8db5
Publicover <jameslpublicover@gmail.com>
2016-03-02 16:56:20 -0500

    got link to display all transactions.

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,10 @@ Rails.application.routes.draw do
   resources :accounts
 
   root 'accounts#index'
-  get 'accounts/index'
-  post 'accounts/index'
+  get 'dashboard' => 'accounts#index'
+  # post 'accounts/index'
+
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
